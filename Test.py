26ea28665162dfc4294a8d577531d1701f6e9325
Mohamed ElBeltagy <Mohamed_elbeltagy@gmail.com>
2021-08-16 22:34:26 +0200

    First commit

---
+++ Test.py
@@ -0,0 +1,31 @@
+# evaluate an ridge regression model on the dataset
+from numpy import mean
+from numpy import std
+from numpy import absolute
+from pandas import read_csv
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedKFold
+from sklearn.linear_model import Ridge
+# load the dataset
+url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/housing.csv'
+dataframe = read_csv(url, header=None)
+data = dataframe.values
+X, y = data[:, :-1], data[:, -1]
+# define model
+model = Ridge(alpha=1.0)
+# define model evaluation method
+cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+# evaluate model
+scores = cross_val_score(model, X, y, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1)
+# force scores to be positive
+scores = absolute(scores)
+print('Mean MAE: %.3f (%.3f)' % (mean(scores), std(scores)))
+
+# fit model
+model.fit(X, y)
+# define new data
+row = [0.00632,18.00,2.310,0,0.5380,6.5750,65.20,4.0900,1,296.0,15.30,396.90,4.98]
+# make a prediction
+yhat = model.predict([row])
+# summarize prediction
+print('Predicted: %.3f' % yhat)
